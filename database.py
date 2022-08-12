e0cc34688d0c70dfe9f84522570d07ffcef7a22a
msmsa2001 <msmsa251001@gmail.com>
2022-08-12 14:30:49 +0530

    add new database link

--- database.py
+++ database.py
@@ -6,7 +6,10 @@ import os
 
 
 
-engine=create_engine("postgresql://xvbxecklfindav:4f5a1d204c2188a414f36ee4838616b3c1482d8f4f26a972aacef3bc29a449a0@ec2-34-233-115-14.compute-1.amazonaws.com:5432/d3ej94n4em2egg")
+# engine=create_engine("postgresql://xvbxecklfindav:4f5a1d204c2188a414f36ee4838616b3c1482d8f4f26a972aacef3bc29a449a0@ec2-34-233-115-14.compute-1.amazonaws.com:5432/d3ej94n4em2egg")
+
+
+engine=create_engine("postgresql://fvciisfzvmwxyw:69769d7f8ea33d4a4a90029b018460c8f1925ecedf41801d4b84d36f526c2890@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d3ambe67es0omc")
 
 # engine=create_engine("postgresql+psycopg2://postgres:ali123@localhost:5432/Employee",echo=True)
 
